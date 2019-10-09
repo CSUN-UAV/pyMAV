ed1495df0487d6bb69e497503781fe4a779ff736
CC <uavcsun@gmail.com>
2019-10-09 18:44:20 +0000

    commuincation from odroid to pixracer through python

--- test_messaging/messagin.py
+++ test_messaging/messagin.py
@@ -2,9 +2,18 @@
 
 from pymavlink import mavutil
 
-the_connection = mavutil.mavlink_connection('/dev/ttyUSB0')
-
+the_connection = mavutil.mavlink_connection('/dev/ttyUSB0', 57600)
+print("waiting")
 # Wait for the first heartbeat 
 #   This sets the system and component ID of remote system for the link
 the_connection.wait_heartbeat()
+print("test")
 print("Heartbeat from system (system %u component %u)" % (the_connection.target_system, the_connection.target_system))
+
+
+try:
+	alt = the_connection_messages['GPs_RAW_INT'].alt
+	timestamp = the_connection.time_since('GPS_RAW_INT')
+except:
+	print("NO GPS")
+
